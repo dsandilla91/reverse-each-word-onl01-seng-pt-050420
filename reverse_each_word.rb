d5040d4c7082748aaf3e58e205ba2f1a3867926e
Deborah Santillan <s.debb@yahoo.com>
2020-05-15 04:56:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-words = ["Hello there, and how are you?"]
-def reverse_each_word 
-  words.reverse_each {|words|} str += #{words}} 
+# words = ["Hello there, and how are you?"]
+# def reverse_each_word 
+#   words.reverse_each {|words|} str += #{words}} 
   
