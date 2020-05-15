bbbbd7885afcf6d521a8da1a13451cc83ab33d75
Deborah Santillan <s.debb@yahoo.com>
2020-05-15 04:59:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 # words = ["Hello there, and how are you?"]
 # def reverse_each_word 
 #   words.reverse_each {|words|} str += #{words}} 
+  
   def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   new_array = []
